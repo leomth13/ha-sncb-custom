f775fd571972fb087deac515688deba861d31fcc
leomth13 <117120804+leomth13@users.noreply.github.com>
2026-08-25 18:52:44 +0200

    Refactor binary sensor name attribute
    Updated the name attribute to remove 'Départ' for clarity.

--- custom_components/sncb_train/binary_sensor.py
+++ custom_components/sncb_train/binary_sensor.py
@@ -36,7 +36,7 @@ class ArrivedFromBinarySensor(
         super().__init__(coordinator)
         self._entry = entry
         self._attr_unique_id = f"{entry.entry_id}_arrived_from"
-        self._attr_name = f"Départ · À quai {coordinator.station_from.title()}"
+        self._attr_name = f"À quai {coordinator.station_from.title()}"
         self._attr_device_info = DeviceInfo(
             identifiers={(DOMAIN, entry.entry_id)},
             name=coordinator.friendly_name,
