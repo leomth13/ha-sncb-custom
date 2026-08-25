53aaeb48995433be43d81de32632eba4d1b3dad8
leomth13 <117120804+leomth13@users.noreply.github.com>
2026-08-25 18:33:29 +0200

    Add BINARY_SENSOR to platforms in SNCB Train Tracker

--- custom_components/sncb_train/__init__.py
+++ custom_components/sncb_train/__init__.py
@@ -22,7 +22,7 @@ from .coordinator import SncbTrainCoordinator
 
 _LOGGER = logging.getLogger(__name__)
 
-PLATFORMS = [Platform.SENSOR]
+PLATFORMS = [Platform.SENSOR, Platform.BINARY_SENSOR]
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
